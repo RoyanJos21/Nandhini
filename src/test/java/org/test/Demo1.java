3153fc95be1a58c2a6d9038cb105941ffdb79538
RoyanJos21 <royanjos279@gmail.com>
2021-01-11 17:05:22 +0530

    added

---
+++ src/test/java/org/test/Demo1.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class Demo1 {
+	
+	
+public static void main(String[] args) {
+	String s="1234";
+    System.out.println(s);
+    System.out.println("Manual testing");
+	
+}
+	
+	
+	
+	
+	
+	
+}
