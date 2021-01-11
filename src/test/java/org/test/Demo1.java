ead2bcffe53a23594404e054cfa753f6dbd0e742
RoyanJos21 <royanjos279@gmail.com>
2021-01-11 17:34:02 +0530

    changed

--- src/test/java/org/test/Demo1.java
+++ src/test/java/org/test/Demo1.java
@@ -4,9 +4,11 @@ public class Demo1 {
 	
 	
 public static void main(String[] args) {
-	String s="1234";
+	String s="11111";
     System.out.println(s);
+    System.out.println("selenium");
     System.out.println("Manual testing");
+    System.out.println("java");
 	
 }
 	
